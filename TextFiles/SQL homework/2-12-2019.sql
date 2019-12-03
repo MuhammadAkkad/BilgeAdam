4235e9856737f9a890dc552545adac692b67e6ac
Muhammad AKKAD <mhammad.akkad@gmail.com>
2019-12-03 10:21:14 +0300

    Update 2-12-2019.sql

--- TextFiles/SQL homework/2-12-2019.sql
+++ TextFiles/SQL homework/2-12-2019.sql
@@ -1,18 +1,23 @@
 ﻿use AdventureWorks2016
 
+--**********************************************************************************************
 -- En az 4 tane tabloyu joinleyerek sorguyu getir.
 select * from Production.Product as product
 inner join Production.ProductModel as modal on product.ProductModelID = modal.ProductModelID
 inner join Production.ProductCostHistory as pHistory on product.ProductID = pHistory.ProductID
 inner join Production.TransactionHistory ptranHis on product.ProductID = ptranHis.ProductID
 inner join Production.ProductInventory pInven on product.ProductID = pInven.ProductID
+--**********************************************************************************************
 
-
+--**********************************************************************************************
 -- Fiyatı en pahalı olan ilk 3 fiyatlı ürünler
 select Name, ListPrice from Production.Product
 where ListPrice in
 (select distinct top 3 ListPrice from Production.Product order by ListPrice desc)
+--**********************************************************************************************
 
+
+--**********************************************************************************************
 --begin##Left, Right, Full, Cross joinlerin herbiriyle birer örnek yap.
 
 -- left:
@@ -32,7 +37,10 @@ select * from Sales.SalesTerritory st
 cross join Sales.SalesTerritoryHistory sth   
 
 --End##
+--**********************************************************************************************
 
+
+--**********************************************************************************************
 --hiç sipariş vermemiş müşteriler kimler en az 3 farklı yöntemle yaz
 
 -- left join kullanarak
@@ -42,13 +50,13 @@ where s.CustomerID is null
 
 
 -- not exist
-select *
-from Sales.Customer c
-where not IN (
-   select c.CustomerID
-   from Sales.Customer c inner join Sales.SalesOrderHeader d on d.CustomerID = c.CustomerID
-   where c.CustomerID = d.CustomerID
+SELECT * FROM Sales.Customer c
+where not exists (
+select * from Sales.SalesOrderHeader soh
+where c.CustomerID = soh.CustomerID
 )
+
+
 
 -- right
 select * from Sales.SalesOrderHeader s  left join
@@ -65,18 +73,18 @@ where c.CustomerID not in
 select c.CustomerID from Sales.Customer c
 inner join Sales.SalesOrderHeader h on c.CustomerID = h.CustomerID
 )
+--**********************************************************************************************
 
-
---En az 4 tane tabloyu joinleyerek sorguyu getir.
---Fiyatı en pahalı olan ilk 3 fiyatlı ürünler??
---Left, Right, Full, Cross joinlerin herbiriyle birer örnek yap.
-
---Self join nedir araştır! Mantığını araştır.
+--Self join nedir araştır! Mantığını araştır
 
 
 
---hiç sipariş vermemiş müşteriler kimler en az 3 farklı yöntemle yaz
 
+
+
+
+
+--**********************************************************************************************
 --fiyatı en pahalı olan ürünün rengindeki bütün ürünler MAX ile
 
 select distinct Color,ListPrice from Production.Product 
@@ -84,13 +92,4 @@ where ListPrice in(
 select  max(ListPrice) from Production.Product group by Color
 )
 order by ListPrice desc
-
-insert into AdventureWorks2016.Production.Product (Color,ListPrice) Values ('ahmar','123456789')
-
-INSERT INTO Production.Product (Color)VALUES('PURPLE')
-
-INSERT INTO Production.Product
-              (Color, ListPrice)
-  VALUES      ('poprble', 09999999)
-
-   
+--**********************************************************************************************
