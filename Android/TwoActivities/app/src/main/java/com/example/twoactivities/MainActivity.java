ecf5497b62ae6bbd1396a000a07396fe91b5ef03
Muahammad <mhammad.akkad@gmail.com>
2019-12-27 20:24:57 +0300

    not big deal

---
+++ Android/TwoActivities/app/src/main/java/com/example/twoactivities/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.twoactivities;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+
+public class MainActivity extends AppCompatActivity {
+    private EditText mMessageEditText;
+
+    public static final String EXTRA_MESSAGE =
+            "twoactivities.extra.MESSAGE";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mMessageEditText = findViewById(R.id.editText_main);
+    }
+
+    public void launchSecondActivity(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        String message = mMessageEditText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
