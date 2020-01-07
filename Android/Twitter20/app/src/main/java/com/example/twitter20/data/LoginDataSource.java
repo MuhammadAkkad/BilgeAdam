3ac3ba84ef0419125d27ba1554e198b7ab1540fe
Muahammad <mhammad.akkad@gmail.com>
2020-01-08 01:41:55 +0300

    fixed login activity

--- Android/Twitter20/app/src/main/java/com/example/twitter20/data/LoginDataSource.java
+++ Android/Twitter20/app/src/main/java/com/example/twitter20/data/LoginDataSource.java
@@ -1,10 +1,5 @@
 package com.example.twitter20.data;
 
-import android.widget.EditText;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import com.example.twitter20.R;
 import com.example.twitter20.data.model.LoggedInUser;
 
 import java.io.IOException;
@@ -12,9 +7,7 @@ import java.io.IOException;
 /**
  * Class that handles authentication w/ login credentials and retrieves user information.
  */
-public class LoginDataSource  extends AppCompatActivity {
-    final EditText usernameTEXT = findViewById(R.id.username);
-    final EditText passward = findViewById(R.id.password);
+public class LoginDataSource {
 
     public Result<LoggedInUser> login(String username, String password) {
 
@@ -23,7 +16,7 @@ public class LoginDataSource  extends AppCompatActivity {
             LoggedInUser fakeUser =
                     new LoggedInUser(
                             java.util.UUID.randomUUID().toString(),
-                            usernameTEXT.getText().toString());
+                            username);
             return new Result.Success<>(fakeUser);
         } catch (Exception e) {
             return new Result.Error(new IOException("Error logging in", e));
